f76967d357e63a37abc876f755eacd26e3fa8a68
CahyaGans <56258686+CahyaGanteng123@users.noreply.github.com>
2019-10-09 18:06:52 +0700

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,11 @@
+echo "Menginstall Bahan Sabar Ajg..."
+sleep 4
+pkg update && pkg upgrade
+pkg install git
+pkg install curl
+pkg install libcurl
+pkg install openssh
+pkg install toilet
+pkg install figlet
+pkg install ruby
+gem install lolcat
